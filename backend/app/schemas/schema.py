b3280cbec127acb9125b7d25b163f8ac70f99d4f
Thameem <muhammedthameempp@gmail.com>
2025-10-21 11:25:27 +0530

    schema updated

--- backend/app/schemas/schema.py
+++ backend/app/schemas/schema.py
@@ -2,34 +2,8 @@ from pydantic import BaseModel, ConfigDict
 from typing import List , Optional, Dict
 from datetime import datetime
 
-# chats
 
-'''class chatRequest(BaseModel):
-    prompt: str
-
-class chatRead(BaseModel):
-    id: int
-    prompt: str
-
-class chatResponse(BaseModel):
-    id: int
-    prompt: str
-    response: str
-    created_at: datetime
-             
-    class config:
-        orm_mode = True
-
-class chatHistoryItsem(BaseModel):
-    user_id: int
-    prompt: List[chatRequest] = []
-
-
-chat_history_store: Dict[str, List[chatRequest]] = {}'''
-
-
-
-#  ------------new shcema 
+#  ------------new schema 
 
 
 class ChatMessageBase(BaseModel):
@@ -47,7 +21,7 @@ class ChatMessageRead(ChatMessageBase):
     model_config = ConfigDict(from_attributes=True)
 
 class ChatSessionBase(BaseModel):
-    # Add session metadata here if any
+ 
     pass
 
 class ChatSessionRead(ChatSessionBase):
